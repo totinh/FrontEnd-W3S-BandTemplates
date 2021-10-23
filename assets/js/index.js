81fd231b0cc60b7442c6f45aece9ae9f8765b080
unknown <nangmuaha0805@gmail.com>
2021-10-23 20:57:19 +0700

    Commit 23-10-2021

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,33 @@ modal.addEventListener('click',hideBuyTickets) // Nhan vao vung trong dong form 
 modalContainer.addEventListener('click',function (event){
     event.stopPropagation()
 })
+
+/* Mobile Menu */ 
+var header = document.getElementById('header');
+var mobileMenu = document.getElementById('mobile-menu');
+var headerHeight = header.clientHeight; // Lấy chiều cao của header là 46px
+
+// Đóng - Mở mobile menu
+mobileMenu.onclick = function(){
+    var isClosed = header.clientHeight === headerHeight;
+    if (isClosed){
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }
+}
+
+// Tự động đóng khi chọn phần tử trong menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+for (var i=0; i<menuItems.length;i++){
+    var menuItem = menuItems[i];
+
+    menuItem.onclick = function(event){
+        var isParenMenu = this.nextElementSibling && this.nextElementSibling.classList.contains('subnav');
+        if(isParenMenu){
+            event.preventDefault();
+        } else {
+            header.style.height = null;
+        }
+    }
+}
